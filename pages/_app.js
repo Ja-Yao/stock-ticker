14104b7fcb91e9b4bc7cc39b977223533875c2a7
Ja-Yao <jtyaos@gmail.com>
2022-05-08 00:15:06 -0400

    Update styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4285F4',
+      contrastText: '#1D2029',
+    }
+  },
+  typography: {
+    fontFamily: [
+      'Visby Round CF',
+      'Quincy CF',
+      '-apple-system',
+      'Roboto',
+      'sans-serif'
+    ].join(','),
+  },
+  overrides: {
+    
+  }
+})
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
-
-export default MyApp
